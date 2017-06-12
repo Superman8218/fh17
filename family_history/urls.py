cbdaddce369c92aa0f4e91b614ab60c669d696ba
Zac Yauney <zac.yauney@gmail.com>
2017-06-12 14:27:42 -0700

    Make Jessica's templates Django compatible

--- family_history/urls.py
+++ family_history/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from views import dashboard
+from views import dashboard, base
 
 urlpatterns = [
+        url(r'^$', base, name='base'),
         url(r'^dashboard$', dashboard, name='dashboard'),
 ]
--- family_history/views.py
+++ family_history/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+def base(request):
+    return render(request, 'family_history/fh.html')
 
 def dashboard(request):
     return render(request, 'family_history/dashboard.html')
